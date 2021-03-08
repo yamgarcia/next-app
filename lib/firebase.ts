6f3416311aba260b45671b0df7531d7cd9cf967e
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2021-03-07 20:15:41 -0800

    firebase API setup

---
+++ lib/firebase.ts
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+//connects app to the cloud
+const firebaseConfig = {
+  apiKey: "AIzaSyAcon_mGwLXkSuoDWMtCxbhnmxpx4Tzpa8",
+  authDomain: "marcosnextfireblog.firebaseapp.com",
+  projectId: "marcosnextfireblog",
+  storageBucket: "marcosnextfireblog.appspot.com",
+  messagingSenderId: "540744487548",
+  appId: "1:540744487548:web:ccb9ab8223cf72589a2ff9",
+  measurementId: "G-RQZVQWHK6S",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
