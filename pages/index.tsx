0db1714b3b290249936578334d5a5cf6d1aaffb7
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2021-03-08 21:35:00 -0800

    Dinamic routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <h1>Hello world</h1>
+      <Link
+        prefetch={false}
+        href={{
+          pathname: "/[username]",
+          query: { username: "jeffd23" },
+        }}
+      ></Link>
     </>
   );
 }
