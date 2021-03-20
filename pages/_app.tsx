1ab8ad46ae64d48e220b84eeceb4eff2c336fd19
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2021-03-19 23:31:38 -0700

    Add ContextAPI feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,16 @@ import Navbar from "../components/Navbar";
 
 import { Toaster } from "react-hot-toast";
 
+import { UserContext } from "../lib/context";
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
-      <Toaster />
+      <UserContext.Provider value={{ user: {}, username: "jeff" }}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Toaster />
+      </UserContext.Provider>
     </>
   );
 }
--- pages/enter.js
+++ pages/enter.js
@@ -1,8 +1,39 @@
-export default function EnterPage({}) {
-  return(
+import {auth, googleAuthProvider} from '../lib/firebase';
+// import google from "../public/google.pgn"
+import {useContext} from "react";
 
+import { UserContext } from "../lib/context";
+
+export default function Enter(props) {
+
+  const {user, username } = useContext(UserContext)
+
+  return(
   <main>
-    <h1>Sing up</h1>
+   {user ? 
+        !username ? <UsernameForm /> : <SignOutButton /> : <SignInButton />
+      }
   </main>
   )
+}
+
+
+function SignInButton(){
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider)
+  }
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={'/google.png'} alt="google"/> Sign in with Google
+    </button>
+  )
+}
+
+function SignOutButton(){
+  return <button onClick={() => auth.signOut()}> Sign Out </button>
+}
+
+function UsernameForm(){
+
 }
