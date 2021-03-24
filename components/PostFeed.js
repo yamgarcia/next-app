09cb63fddbec53f02c03e6635012cb92f921f644
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2021-03-23 23:16:58 -0700

    Profile page w/ rendered HTML

---
+++ components/PostFeed.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export default function PostFeed({ posts, admin }) {
+    return posts ? posts.map((post)=> <PostItem post={post} key={post.slug} admin={admin} />) : <></>;
+}
+
+function PostItem ({post, admin = false}) {
+    const wordCount = post?.content.trim().split(/\s+/g).lenght;
+    const minutesToRead = (wordCount / 100 + 1).toFixed(0);
+
+    return (
+        <div className="card">
+            <Link href={`/${post.username}`}>
+            <a>
+                <strong>By @{post.username}</strong>
+            </a>
+            </Link>
+
+            <Link href={`/${post.username}/${post.slug}`}>
+           <h2>
+               <a> {post.title} </a>
+           </h2>
+            </Link>
+
+            <footer>
+                <span>
+                    {wordCount} words. {minutesToRead} min read
+                </span>
+                <span>❤ {post.heartCount} Hearts </span>
+            </footer>
+
+        </div>
+    )
+}
---
+++ components/UserProfile.js
@@ -0,0 +1,12 @@
+export default function UserProfile ({user}){
+    return (
+        <div className="box-center">
+            <img src={user.photoURL} alt="user-photo" className="card-img-center"/>
+            <p>
+                <i>@{user.username}</i>
+            </p>
+            <h1>{user.displayName}</h1>
+
+        </div>
+    )
+}
