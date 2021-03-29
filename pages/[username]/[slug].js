edd8c5bf569530b6cf4aa351f319d3bbc335ce6b
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2021-03-28 23:42:24 -0700

    Fix GetStaticPath

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -36,10 +36,6 @@ export async function getStaticProps({ params }) {
     });
   
     return {
-      // must be in this format:
-      // paths: [
-      //   { params: { username, slug }}
-      // ],
       paths,
       fallback: 'blocking',
     };
